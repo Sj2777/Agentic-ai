e7eeb552e7ccdd1f285f542f7d3f1a4963db6e3a
Sj2777 <sj2777radha@gmail.com>
2025-11-30 01:21:43 +0530

    Delete tavily_search.py

--- tavily_search.py
+++
@@ -1,8 +0,0 @@
-from tavily import TavilyClient
-from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file
-import os
-tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
-response = tavily_client.search(query=query, max_results=max_results)
-
-print(response)
